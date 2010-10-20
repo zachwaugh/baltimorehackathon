94b507614011e76aaa676e15edc8186c12802329
Zach Waugh <zwaugh@gmail.com>
2010-10-19 23:05:42 -0400

    Initial commit. Basic site structure complete

---
+++ js/application.js
@@ -0,0 +1,21 @@
+
+$(function()
+{
+  $('#octopus').click(attack);
+});
+
+
+function attack(event)
+{
+	var octopus = $('#octopus');
+	octopus.css({top: octopus.offset().top, left: octopus.offset().left});
+	var diver = $('#diver');
+	
+	$('#octopus').animate({left: diver.offset().left - 50, top: diver.offset().top + 300}, {duration: 1000, complete: function()
+	{
+		$('#octopus, #diver').animate({left: 100, top: 5000}, function()
+		{
+			$(this).remove();
+		});
+	}});
+}
